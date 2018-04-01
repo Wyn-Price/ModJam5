d0ff150e8cfdd64174bb861e5bcdb6d9ba16a143
Wyn Price <wynprice999@gmail.com>
2018-04-01 01:31:23 +0100

    Made Boots Recipe its own file

---
+++ src/main/java/com/wynprice/modjam5/common/registries/WorldPaintRecipes.java
@@ -0,0 +1,18 @@
+package com.wynprice.modjam5.common.registries;
+
+import com.wynprice.modjam5.WorldPaint;
+import com.wynprice.modjam5.common.recipes.RecipePaintBoots;
+
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@EventBusSubscriber(modid=WorldPaint.MODID)
+public class WorldPaintRecipes {
+	
+	@SubscribeEvent
+	public static void onRecipeRegister(RegistryEvent.Register<IRecipe> event) {
+		event.getRegistry().registerAll(new RecipePaintBoots());
+	}	
+}
